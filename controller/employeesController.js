38de69799a58ebd3549d851e39f0ca66b50b5c39
advancedwebcitp4316@gmail.com <advancedwebcitp4316@gmail.com>
2024-03-03 06:44:53 -0600

    update is working

--- controller/employeesController.js
+++ controller/employeesController.js
@@ -1,5 +1,6 @@
 import Employees from "../model/employeesModel.js"
 
+//create a new record
 export const create = async(req, res) => {
     try {
         const employeeData = new Employees(req.body)
@@ -20,7 +21,7 @@ export const create = async(req, res) => {
 
 
 
-
+//fetch a record
 export const fetch = async (req, res) => {
     try {
         //res.json("Hello World")
@@ -31,5 +32,23 @@ export const fetch = async (req, res) => {
         res.status(200).json(employees)
     } catch(error) {
         res.status(500).json({ error: "Internal Server Error"})
+    }
+}
+
+//update a record
+export const update = async(req, res) => {
+    try {
+        const id = req.params.id;
+        const employeeExist = await Employees.findOne({_id:id})
+
+        if(!employeeExist) {
+            return res.status(404).json({message: "Employee not found"})
+        }
+
+        const updateEmployee = await Employees.findByIdAndUpdate(id, req.body, {new: true})
+        res.status(201).json(updateEmployee)
+
+    } catch (error) {
+        res.status(500).json({error: "Internal Server Error"})
     }
 }
--- routes/employeesRoute.js
+++ routes/employeesRoute.js
@@ -1,8 +1,9 @@
 import express from "express"
-import { create, fetch } from "../controller/employeesController.js"
+import { create, fetch, update } from "../controller/employeesController.js"
 const route = express.Router()
 
 route.get("/fetch", fetch)
 route.post("/create", create)
+route.put("/update/:id", update)
 
 export default route
