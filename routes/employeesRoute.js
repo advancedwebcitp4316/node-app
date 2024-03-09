d14a72f00841b12804a6c273a164b9b51966ae77
advancedwebcitp4316@gmail.com <advancedwebcitp4316@gmail.com>
2024-03-09 15:45:36 -0600

    getting single employee works

--- routes/employeesRoute.js
+++ routes/employeesRoute.js
@@ -1,9 +1,10 @@
 import express from "express"
-import { create, fetch, update, deleteEmployee } from "../controller/employeesController.js"
+import { create, fetch, update, deleteEmployee, findEmployee } from "../controller/employeesController.js"
 const route = express.Router()
 
 route.get("/fetch", fetch)
 route.post("/create", create)
 route.put("/update/:id", update)
 route.delete("/delete/:id", deleteEmployee)
+route.get("/fetch/:id", findEmployee)
 export default route
